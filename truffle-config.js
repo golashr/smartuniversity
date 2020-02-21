ca05fafd524241aed843375a850d84ce89e8851c
golashr <rahul.g@ledgerium.net>
2020-02-21 23:46:32 +1100

    upgrade solidity compiler and rebuilt

---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: 'flinders01.ledgerium.io',
+      port: 8545,
+      network_id: '2019', // Ledgerium public network
+      from: "0x07095df464603b6f225a5f19fcaec155d7489336"
+      }
+  }
+}
